8b9eb8f63515aead07bdcabd53e6c842abbb42dd
vitor <vpbohn@gmail.com>
2023-07-07 19:14:09 -0300

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <main>
+    <div class="wrapper">
+      <HelloWorld msg="Vue + tailwind + router + store" />
+    </div>
+  </main>
+</template>
